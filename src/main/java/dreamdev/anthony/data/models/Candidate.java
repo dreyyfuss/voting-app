7db32ccee946ba4e1c3263aec11c06766fa5b145
Anthony Alikah <a.o.alikah@gmail.com>
2026-03-31 15:27:50 +0100

    Initial commit

---
+++ src/main/java/dreamdev/anthony/data/models/Candidate.java
@@ -0,0 +1,10 @@
+package dreamdev.anthony.data.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Candidate {
+    @Id
+    private String id;
+    private String userId;
+    private String electionId;
+}
---
+++ src/main/java/dreamdev/anthony/data/models/Election.java
@@ -0,0 +1,17 @@
+package dreamdev.anthony.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document
+public class Election {
+    @Id
+    private String id;
+    private String electionName;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
