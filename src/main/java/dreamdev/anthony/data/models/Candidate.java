0db0f6a4ea011858a8a5f308e2bc65384f91d521
Anthony Alikah <a.o.alikah@gmail.com>
2026-03-31 15:30:03 +0100

    add data and document annotations

--- src/main/java/dreamdev/anthony/data/models/Candidate.java
+++ src/main/java/dreamdev/anthony/data/models/Candidate.java
@@ -1,7 +1,10 @@
 package dreamdev.anthony.data.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+@Data
+@Document
 public class Candidate {
     @Id
     private String id;
--- src/main/java/dreamdev/anthony/data/models/Voter.java
+++ src/main/java/dreamdev/anthony/data/models/Voter.java
@@ -1,7 +1,11 @@
 package dreamdev.anthony.data.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document
 public class Voter {
     @Id
     private String id;
